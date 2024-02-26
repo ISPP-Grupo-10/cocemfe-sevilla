16fc4c91058bb2a9323885346fd3b019b9e2472b
guillegomezz8 <guigomrom@alum.us.es>
2024-02-26 16:53:19 +0100

    node modules desintalado

--- codenfe_sevilla/node_modules/bootstrap5/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Button } from "./components/Buttons/Button";
-import { SaveButton } from "./components/Buttons/SaveButton";
-import { Navbar } from "./components/Layout/Navbar";
-import { ThemeProvider } from "./themes";
-
-export { Button, SaveButton, Navbar, ThemeProvider };
--- codenfe_sevilla/node_modules/bootstrap5/src/themes/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ThemeProvider as Theme } from "../hooks/theme";
-
-export const ThemeProvider: React.FC = ({ children }) => {
-  return <Theme>{children}</Theme>;
-};
