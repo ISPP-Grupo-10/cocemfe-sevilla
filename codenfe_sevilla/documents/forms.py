efa8e537cbd1fcd434ae55b46de9534ed990b7cc
dancorfon <dancorfon@alum.us.es>
2024-02-26 19:39:28 +0100

    feat: Modificacion Documentos

--- codenfe_sevilla/documents/forms.py
+++ codenfe_sevilla/documents/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Document
+from professionals.models import Professional
 
 class PDFUploadForm(forms.ModelForm):
+
+    professionals = forms.ModelMultipleChoiceField(queryset=Professional.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Document
-        fields = ['name', 'pdf_file', 'end_date']
+        fields = ['name', 'pdf_file', 'end_date', 'professionals']
         widgets = {
             'end_date': forms.DateInput(attrs={'type': 'date'})
         }
