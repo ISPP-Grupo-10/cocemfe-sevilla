fd67449e4470d28934b9484bbcc84d4df9c4a702
guillegomezz8 <guigomrom@alum.us.es>
2024-02-26 17:18:05 +0100

    feat: conflictos done

--- codenfe_sevilla/codenfe_sevilla/urls.py
+++ codenfe_sevilla/codenfe_sevilla/urls.py
@@ -22,9 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("documents/", docsViews.docsList),
     path('', include('base.urls', namespace='base')),
-    path('pdf/', include('documents.urls')),
+    path('documents/', include('documents.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- codenfe_sevilla/documents/urls.py
+++ codenfe_sevilla/documents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import upload_pdf, view_pdf
+from .views import upload_pdf, view_pdf, docsList
 
 urlpatterns = [
+    path("", docsList, name="docsList"),
     path('upload_pdf', upload_pdf, name='upload_pdf'),
     path('view_pdf/<int:pk>/', view_pdf, name='view_pdf'),
 
