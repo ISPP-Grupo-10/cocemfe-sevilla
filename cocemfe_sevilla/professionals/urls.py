d728479555cd9f48828f0f8558a7c9981b89f63a
Tadeo Cabrera <113366030+tcabgom@users.noreply.github.com>
2024-03-01 15:14:04 +0100

    fix: Update urls.py

--- cocemfe_sevilla/professionals/urls.py
+++ cocemfe_sevilla/professionals/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import professional_list, ProfessionalDetailView, ProfessionalUpdateView
+from .views import professional_list, professional_detail_view, professional_update_view
 
 urlpatterns = [
     path('professionals/', professional_list, name='professional_list'),
-    path('professionals/<int:pk>/', ProfessionalDetailView.as_view(), name='professional_detail'),
-    path('professionals/<int:pk>/edit/', ProfessionalUpdateView.as_view(), name='professional_edit'),
+    path('professionals/<int:pk>/', professional_detail_view, name='professional_detail'),
+    path('professionals/<int:pk>/edit/', professional_update_view, name='professional_edit'),
 ]
-
