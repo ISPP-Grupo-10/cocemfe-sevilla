a2fd059972de566e6c43a5ff44bbf2947566a5c1
Ignarrman <nacho.arromant@gmail.com>
2024-03-01 18:42:32 +0100

    conf: Arregla las url de professionals

--- cocemfe_sevilla/professionals/urls.py
+++ cocemfe_sevilla/professionals/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import professional_list, professional_detail_view, professional_update_view
 
 urlpatterns = [
-    path('professionals/', professional_list, name='professional_list'),
-    path('professionals/<int:pk>/', professional_detail_view, name='professional_detail'),
-    path('professionals/<int:pk>/edit/', professional_update_view, name='professional_edit'),
+    path('', professional_list, name='professional_list'),
+    path('<int:pk>/', professional_detail_view, name='professional_detail'),
+    path('<int:pk>/edit/', professional_update_view, name='professional_edit'),
 ]
