2fa4e5860893cb99aa09f770a25f602d5381a139
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-03-01 11:42:36 +0100

    conf: language code y time zone a español

--- codenfe_sevilla/codenfe_sevilla/settings.py
+++ codenfe_sevilla/codenfe_sevilla/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-es"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "GMT+1"
 
 USE_I18N = True
 
