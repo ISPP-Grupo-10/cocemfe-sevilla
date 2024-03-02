7d0208e0eb9e85018b78fe1cc7edf10b275876b2
Ignacio Gonzalez <igngongon2@alum.us.es>
2024-03-02 12:37:03 +0100

    fix: Resolución de conflictos

--- cocemfe_sevilla/professionals/views.py
+++ cocemfe_sevilla/professionals/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DeleteView, DetailView, UpdateView
+
 from .models import Professional
 from .forms import ProfessionalForm
 
@@ -11,6 +12,9 @@ class EditUserView(View):
         professional = get_object_or_404(Professional, id=pk)
         form = ProfessionalForm(instance=professional)
         return render(request, 'professional_detail.html', {'form': form, 'professional': professional})
+
+    def professional_update_view(request, pk):
+        professional = get_object_or_404(Professional, pk=pk)
 
     def post(self, request, pk):
         professional = get_object_or_404(Professional, id=pk)
@@ -24,27 +28,21 @@ class EditUserView(View):
 
 def professional_list(request):
     professionals = Professional.objects.all()
-
     name_filter = request.GET.get('name', '')
     if name_filter:
         professionals = professionals.filter(name__icontains=name_filter)
 
-    surname_filter = request.GET.get('surname', '')
-    if surname_filter:
-        professionals = professionals.filter(surname__icontains=surname_filter)
+    return render(request, 'professional_list.html', {'professionals': professionals})
 
-    license_number_filter = request.GET.get('license_number', '')
-    if license_number_filter:
-        professionals = professionals.filter(license_number__icontains=license_number_filter)
 
-    organization_filter = request.GET.get('organization', '')
-    if organization_filter:
-        professionals = professionals.filter(organizations__name__icontains=organization_filter)
+def delete_professional(request, id):
+    professional = get_object_or_404(Professional, id=id)
+    print(professional)
 
-    return render(request, 'professional_list.html', {
-        'professionals': professionals,
-        'name_filter': name_filter,
-        'surname_filter': surname_filter,
-        'license_number_filter': license_number_filter,
-        'organization_filter': organization_filter,
-    })
+    if request.method == 'POST':
+        professional.delete()
+
+    professionals = Professional.objects.all()
+
+    return render(request, 'professional_list.html', {'professionals': professionals})
+
