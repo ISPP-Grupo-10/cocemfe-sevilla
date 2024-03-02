03032b6f657f9341ec2b451978dfc1c9642697aa
Ignarrman <nacho.arromant@gmail.com>
2024-03-02 11:45:58 +0100

    feat: Añade Frontend Actualizar y Visualizar Profesional #82
    Faltan algunos atributos por el backend

--- cocemfe_sevilla/professionals/models.py
+++ cocemfe_sevilla/professionals/models.py
@@ -5,8 +5,6 @@ from organizations.models import Organization
 
 
 class Professional(AbstractUser):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
     telephone_number = models.CharField(max_length=15)
     license_number = models.CharField(max_length=20)
     organizations = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='professionals', null=True)
--- cocemfe_sevilla/professionals/views.py
+++ cocemfe_sevilla/professionals/views.py
@@ -1,40 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import DetailView, UpdateView
 from .models import Professional
 from .forms import ProfessionalForm
 
 
-def professional_detail_view(request, pk):
-    professional = get_object_or_404(Professional, pk=pk)
+class EditUserView(View):
+    def get(self, request, pk):
+        professional = get_object_or_404(Professional, id=pk)
+        form = ProfessionalForm(instance=professional)
+        return render(request, 'professional_detail.html', {'form': form, 'professional': professional})
 
-    # Puedes agregar cualquier otra lógica que necesites aquí
-
-    context = {
-        'professional': professional,
-        'request': request,
-    }
-
-    return render(request, 'professional_detail.html', context)
-
-def professional_update_view(request, pk):
-    professional = get_object_or_404(Professional, pk=pk)
-
-    if request.method == 'POST':
+    def post(self, request, pk):
+        professional = get_object_or_404(Professional, id=pk)
         form = ProfessionalForm(request.POST, instance=professional)
         if form.is_valid():
             form.save()
-            return redirect('professional_detail', pk=pk)  # Ajusta el nombre de la URL según tu configuración
-    else:
-        form = ProfessionalForm(instance=professional)
-
-    context = {
-        'form': form,
-        'professional': professional,
-        'request': request,
-    }
-
-    return render(request, 'professional_form.html', context)
+            return redirect('/?message=Profesional editado&status=Success')
+        else:
+            return render(request, 'professional_detail.html', {'form': form, 'professional': professional})
 
 
 def professional_list(request):
