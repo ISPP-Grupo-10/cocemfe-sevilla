bb0c41f3bea3eea44a177642fc332e40eef48925
PabloPinMat <pablopinomateo27@gmail.com>
2024-03-04 20:47:39 +0100

    feat: validate enforce exact 5-digit zip code in Organization model

--- cocemfe_sevilla/organizations/models.py
+++ cocemfe_sevilla/organizations/models.py
@@ -9,11 +9,13 @@ class Organization(models.Model):
         message='El número de teléfono debe tener exactamente 9 dígitos.',
         code='invalid_phone_number'
     )
+
     name = models.CharField(max_length=255)
     telephone_number = models.CharField(max_length=9, validators=[phone_number_validator])
     address = models.CharField(max_length=255)
     email = models.EmailField()
-    zip_code = models.IntegerField()
+    zip_code = models.IntegerField(validators=[RegexValidator(regex=r'^\d{5}$', message='El codigo postal debe tener 5 dígitos')])
+
 
 
     def __str__(self):
