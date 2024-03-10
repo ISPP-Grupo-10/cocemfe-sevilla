d09e065aae32973af70bd2dcff33830dad2a85c9
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-03-10 11:50:14 +0100

    fix: corregido enumerado relevance

--- cocemfe_sevilla/suggestions/models.py
+++ cocemfe_sevilla/suggestions/models.py
@@ -3,14 +3,14 @@ from documents.models import Document
 from professionals.models import Professional
 
 class Suggestion(models.Model):
-    RELEVANCE_CHOICES = (
-        ('urgente', 'Urgente'),
-        ('importante', 'Importante'),
-        ('poco', 'Poco importante'),
+    RELEVANCE = (
+        ('Muy importante', 'Muy importante'),
+        ('Importante', 'Importante'),
+        ('Poco importante', 'Poco importante'),
     )
     main = models.CharField(max_length=255)
     justification = models.TextField()
-    relevance = models.CharField(max_length=20, choices=RELEVANCE_CHOICES)
+    relevance = models.CharField(max_length=20, choices=RELEVANCE)
     section = models.CharField(max_length=10)
     page = models.IntegerField()
     date = models.DateField()
