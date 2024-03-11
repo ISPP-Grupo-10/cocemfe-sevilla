027930cd109806e29dcfc13e5864293197b67190
dancorfon <dancorfon@alum.us.es>
2024-03-11 19:53:13 +0100

    feat: Redireccion pagina

--- cocemfe_sevilla/cocemfe_sevilla/settings.py
+++ cocemfe_sevilla/cocemfe_sevilla/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     "chat_messages",
     "documents",
     "professionals",
@@ -49,11 +50,19 @@ INSTALLED_APPS = [
     "base"
 ]
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    # Agrega otros dominios permitidos si es necesario
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
