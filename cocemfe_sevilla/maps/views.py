8895293943a4f3456bc4d0adc3c9d4ce219bafd5
eloyjm <eloyjm93@gmail.com>
2024-03-13 20:52:56 +0100

    feat: mapa centrado en sevilla y chincheta

--- cocemfe_sevilla/maps/views.py
+++ cocemfe_sevilla/maps/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+import folium
 
 def map_index(request):
-    return render(request, 'maps_index.html')
+    # Creamos un mapa centrado en una ubicación específica
+    mapa = folium.Map(location=[37.3896, -5.9845], zoom_start=10, zoom_control=True, scrollWheelZoom=True)
+
+    # Agregamos marcadores al mapa
+    folium.Marker(location=[37.3896, -5.9845], popup='Sevilla').add_to(mapa)
+
+    # Convertimos el mapa a HTML
+    mapa_html = mapa._repr_html_()
+
+    # Renderizamos la plantilla con el mapa
+    return render(request, 'maps_index.html', {'mapa_html': mapa_html})
