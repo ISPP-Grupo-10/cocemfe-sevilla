3d08af83c3bbbcafe409e7b8fa890708553ce4a8
tcabgom <tcabgom222@gmail.com>
2024-04-05 13:27:56 +0200

    fix: Cobertura Codacy

--- cocemfe_sevilla/professionals/tests.py
+++ cocemfe_sevilla/professionals/tests.py
@@ -433,16 +433,60 @@ class PasswordChangeFormTests(TestCase):
         response = self.client.get(reverse('professionals:update_password'))
         self.assertEqual(response.status_code, 302)
 
-    def test_password_strength_validation(self):
+    def test_password_strength_validation_no_uppercase(self):
         self.client.login(username='testuser', password='old_password')
         form = SecurePasswordChangeForm(user=self.user, data={
             'old_password': 'old_password',
-            'new_password1': 'weakpassword',
-            'new_password2': 'weakpassword',
+            'new_password1': 'new_password123',
+            'new_password2': 'new_password123',
         })
         self.assertFalse(form.is_valid())
         self.assertIn('new_password1', form.errors)
-        self.assertEqual(form.errors['new_password1'], ['La contraseña debe contener al menos una letra mayúscula.'])
+        self.assertEquals(form.errors['new_password1'], ['La contraseña debe contener al menos una letra mayúscula.'])
+
+    def test_password_strength_validation_no_lowercase(self):
+        self.client.login(username='testuser', password='old_password')
+        form = SecurePasswordChangeForm(user=self.user, data={
+            'old_password': 'old_password',
+            'new_password1': 'NEW_PASSWORD123',
+            'new_password2': 'NEW_PASSWORD123',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password1', form.errors)
+        self.assertEquals(form.errors['new_password1'], ['La contraseña debe contener al menos una letra minúscula.'])
+
+    def test_password_strength_validation_short(self):
+        self.client.login(username='testuser', password='old_password')
+        form = SecurePasswordChangeForm(user=self.user, data={
+            'old_password': 'old_password',
+            'new_password1': 'Short_123',
+            'new_password2': 'Short_123',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password1', form.errors)
+        self.assertEquals(form.errors['new_password1'], ['La contraseña debe tener al menos 12 caracteres.'])
+
+    def test_password_strength_validation_no_digits(self):
+        self.client.login(username='testuser', password='old_password')
+        form = SecurePasswordChangeForm(user=self.user, data={
+            'old_password': 'old_password',
+            'new_password1': 'New_Password!!!',
+            'new_password2': 'New_Password!!!',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password1', form.errors)
+        self.assertEquals(form.errors['new_password1'], ['La contraseña debe contener al menos un dígito.'])
+
+    def test_password_strength_validation_no_digits(self):
+        self.client.login(username='testuser', password='old_password')
+        form = SecurePasswordChangeForm(user=self.user, data={
+            'old_password': 'old_password',
+            'new_password1': 'NewPassword12345',
+            'new_password2': 'NewPassword12345',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password1', form.errors)
+        self.assertEquals(form.errors['new_password1'], ['La contraseña debe contener al menos un carácter especial.'])
 
     def test_password_matching_validation(self):
         self.client.login(username='testuser', password='old_password')
