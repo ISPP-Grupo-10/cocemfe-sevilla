032d4b815b45e89444e11d5569cd9cb32f1e4b09
Ignacio Gonzalez <igngongon2@alum.us.es>
2024-04-13 13:48:03 +0200

    feat: Añadido pantalla de estadísticas para cada documento en función de los suggestions que tiene.

--- cocemfe_sevilla/votings/urls.py
+++ cocemfe_sevilla/votings/urls.py
@@ -1,9 +1,11 @@
 # En el archivo urls.py de tu aplicación 'votings'
 from django.urls import path
-from .views import voting_vote
+from .views import voting_statistics, voting_vote
 
 app_name = "votings"
 
 urlpatterns = [
     path('vote/<int:suggestion_id>/', voting_vote, name='voting_vote'),
+    path('statistics/<int:pk>/', voting_statistics, name='voting_statistics'),
+
 ]
--- cocemfe_sevilla/votings/views.py
+++ cocemfe_sevilla/votings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from documents.models import Document
 from suggestions.models import Suggestion
 from datetime import date
 from .models import Voting
@@ -32,3 +33,31 @@ def voting_vote(request, suggestion_id):
         # Si no es una solicitud POST, redirigir a la vista de sugerencias
         suggestion = get_object_or_404(Suggestion, id=suggestion_id)
         return render(request, 'view_suggestion.html', {'suggestion': suggestion})
+    
+    
+def voting_statistics(request, pk):
+    document = Document.objects.get(pk=pk)
+    suggestions = Suggestion.objects.filter(document=document)
+    
+    suggestion_data = []
+    for suggestion in suggestions:
+        votes = Voting.objects.filter(suggestion=suggestion)
+        votes_agree = votes.filter(vote=True).count()
+        votes_disagree = votes.filter(vote=False).count()
+        
+        professionals_voted = set(vote.professional for vote in votes)
+        professionals_remaining = document.professionals.exclude(id__in=[prof.id for prof in professionals_voted]).count()
+        
+        suggestion_data.append({
+            'suggestion': suggestion,
+            'votes_agree': votes_agree,
+            'votes_disagree': votes_disagree,
+            'professionals_remaining': professionals_remaining,
+        })
+    
+    context = {
+        'document': document,
+        'suggestion_data': suggestion_data,
+    }
+    
+    return render(request, 'voting_statistics.html', context)
