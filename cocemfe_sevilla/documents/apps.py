fff30f0d85486385bd4432d0388deab6cbbefd40
antoniopelaezmoreno <antpelmor@alum.us.es>
2024-05-19 15:29:59 +0200

    fix: añade django-q para actualizar los estados automaticamente

--- cocemfe_sevilla/documents/apps.py
+++ cocemfe_sevilla/documents/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DocumentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'documents'
+
+    def ready(self):
+        import documents.tasks  # Importa el archivo tasks.py
