7d915d55f9deb1582e37ef99f48caff66e17ce3b
Ignarrman <nacho.arromant@gmail.com>
2024-05-19 20:07:50 +0200

    fix: Arreglar errores despliegue parte 3

--- cocemfe_sevilla/cocemfe_sevilla/settings.py
+++ cocemfe_sevilla/cocemfe_sevilla/settings.py
@@ -96,7 +96,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "https://cocemfe-4a7kpawtwa-uc.a.run.app",
-    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app/",
+    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app/"
 ]
 
 TEMPLATES = [
