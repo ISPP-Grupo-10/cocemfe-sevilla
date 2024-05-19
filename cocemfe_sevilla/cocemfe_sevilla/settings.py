4af0bb9b10cfd09202c891021aa7c9c31bfaf276
Ignarrman <nacho.arromant@gmail.com>
2024-05-20 00:25:37 +0200

    conf: D O C K E R

--- cocemfe_sevilla/cocemfe_sevilla/settings.py
+++ cocemfe_sevilla/cocemfe_sevilla/settings.py
@@ -90,13 +90,15 @@ ROOT_URLCONF = "cocemfe_sevilla.urls"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "https://cocemfe-4a7kpawtwa-uc.a.run.app",
-    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app"
+    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app",
+    "https://cocemfe-test-4a7kpawtwa-uc.a.run.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "https://cocemfe-4a7kpawtwa-uc.a.run.app",
-    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app"
+    "https://cocemfe-wpl-4a7kpawtwa-no.a.run.app",
+    "https://cocemfe-test-4a7kpawtwa-uc.a.run.app"
 ]
 
 TEMPLATES = [
